76da675552ecacc08094585f28cdee6cea15fcfe
Thamie <84879911+Thamieee@users.noreply.github.com>
2022-08-27 11:35:18 -0300

    fix2?

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from flask import Flask, render_template
 
-revolto = Flask(__name__)
+app = Flask(__name__)
 
-@revolto.route("/")
+@app.route("/")
 def homepage():
     return render_template("homepage.html")
 
-@revolto.route("/contato")
+@app.route("/contato")
 def contato():
     return render_template("contato.html")
 
-@revolto.route("/usuario/<nome_usuario>")
+@app.route("/usuario/<nome_usuario>")
 def usuario(nome_usuario):
     return render_template("usuario.html", username = nome_usuario)
 
 if __name__ == "__main__":
-    revolto.run(debug = True)
+    app.run(debug = True)
