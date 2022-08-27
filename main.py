0520d89456fc7230ba34aa007cce079f157b27b7
Thamie <84879911+Thamieee@users.noreply.github.com>
2022-08-27 15:25:07 -0300

    homepage v0 done

--- main.py
+++ main.py
@@ -14,5 +14,21 @@ def contato():
 def usuario(nome_usuario):
     return render_template("usuario.html", username = nome_usuario)
 
+@app.route("/sobre")
+def sobre():
+    return render_template("sobre.html")
+
+@app.route("/jogos")
+def jogos():
+    return render_template("jogos.html")
+
+@app.route("/blog")
+def blog():
+    return render_template("blog.html")
+
+@app.route("/jogos/oniriko")
+def oniriko():
+    return render_template("oniriko.html")
+
 if __name__ == "__main__":
     app.run(debug = True)
