81016e948963321fd67989eede495fe1867a1a19
Thamie <84879911+Thamieee@users.noreply.github.com>
2022-09-04 09:21:08 -0300

    Flask Application + some HTML's

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'noreply.revoltosoftware@gmail.com'
+app.config['MAIL_PASSWORD'] = 'uyimtytcszuwqcjg'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_DEFAULT_SENDER'] = ('Revolto Software', 'noreply.revoltosoftware@gmail.com')
+mail = Mail(app)
 
 @app.route("/")
+@app.route("/home")
 def homepage():
     return render_template("homepage.html")
 
-@app.route("/contato")
+@app.route("/contato", methods=["GET", "POST"])
 def contato():
+    if request.method == "POST":
+        msg = Message("Teste", recipients = ["noreply.revoltosoftware@gmail.com"])
+        msg.body = "Testando envio de e-mail pelo Flask"
+        mail.send(msg)
+        return "Message sent!"
     return render_template("contato.html")
 
-@app.route("/usuario/<nome_usuario>")
-def usuario(nome_usuario):
-    return render_template("usuario.html", username = nome_usuario)
+# @app.route("/usuario/<nome_usuario>")
+# def usuario(nome_usuario):
+#     return render_template("usuario.html", username = nome_usuario)
 
 @app.route("/sobre")
 def sobre():
